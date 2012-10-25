d07f4eb128a3619147c9c99d3201e561b964d205
Tessier <tessie_g@epitech.eu>
2012-10-25 17:56:37 +0200

    optimize js google calendar

--- public/javascript/show_event_google_calendar.js
+++ public/javascript/show_event_google_calendar.js
@@ -6,11 +6,14 @@ function loop_show_event_google_calendar()
 }
 function show_event()
 {
+    var that = this;
     $.ajax({
 	url:	  '/agenda',
 	dataType: 'html',
 	success: function(data){
-	    $("#event_google_calendar").html(data);
+	    if (that.html == "undefined" || that.html != data)
+		$("#event_google_calendar").html(data);
+	    that.html = data
 	},
 	error: function(){
 	    $("#event_google_calendar").text("Erreur dans la récupération des tâches.");
